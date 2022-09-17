4c40c687063a86baf034e19c743c0e028a1d336f
Adrienne Mota <adriennemota97@gmail.com>
2022-09-17 13:32:00 -0400

    story renderizando a partir de map

--- src/Stories.js
+++ src/Stories.js
@@ -1,65 +1,68 @@
+function Story(prop){
+        return(    
+            <div className="story">
+                <div>
+                    <img src={prop.borda} alt="borda do storie"/>
+                    <img src={prop.imagem} alt="9gag"/>     
+                    </div>
+                    <h1>{prop.nome}</h1>
+            </div>
+        )
+}
+
 export default function Stories(){
+    
+    const arrStory = [
+        {
+            borda: "./img/stories_background.jpg",
+            imagem: "./img/stories/9gag 1.png",
+            nome: "9gap"  
+        },
+        {
+            borda: "./img/stories_background.jpg",
+            imagem: "./img/stories/2.png",
+            nome: "barked" 
+        },
+        {
+            borda: "./img/stories_background.jpg",
+            imagem: "./img/stories/3.png",
+            nome: "filo" 
+        },
+        {
+            borda: "./img/stories_background.jpg",
+            imagem: "./img/stories/4.png",
+            nome: "bolinho" 
+        },
+        {
+            borda: "./img/stories_background.jpg",
+            imagem: "./img/stories/5.png",
+            nome: "meowed" 
+        },
+        {
+            borda: "./img/stories_background.jpg",
+            imagem: "./img/stories/6.png",
+            nome: "etbr" 
+        },
+        {
+            borda: "./img/stories_background.jpg",
+            imagem: "./img/stories/7.png",
+            nome: "respondeai" 
+        },
+        {
+            borda: "./img/stories_background.jpg",
+            imagem: "./img/stories/8.png",
+            nome: "wawa" 
+        }
+    ] 
+
     return(
         <div className="stories">
-                <div className="story">
-                    <div>
-                        <img src="./img/stories_background.jpg" alt="borda do storie"/>
-                        <img src="./img/stories/9gag 1.png" alt="9gag"/>     
-                    </div>
-                    <h1>9gap</h1>
-                </div>
-                <div className="story">
-                    <div>
-                        <img src="./img/stories_background.jpg" alt="borda do storie"/>
-                        <img src="./img/stories/2.png"/>                    
-                    </div>
-                    <h1>barked</h1>
-                </div>
-                <div className="story">
-                    <div>
-                        <img src="./img/stories_background.jpg" alt="borda do storie"/>
-                        <img src="./img/stories/3.png" alt="folomoderna"/>                    
-                    </div>
-                    <h1>filomoderna</h1>
-                </div>
-                <div className="story">
-                    <div>
-                        <img src="./img/stories_background.jpg" alt="borda do storie"/>
-                        <img src="./img/stories/4.png" alt="memeriago"/>                    
-                    </div>
-                    <h1>9gap</h1>
-                </div>
-                <div className="story">
-                    <div>
-                        <img src="./img/stories_background.jpg" alt="borda"/>
-                        <img src="./img/stories/5.png" alt="meowed"/>                    
-                    </div>
-                    <h1>meowed</h1>
-                </div>
-                <div className="story">
-                    <div>
-                        <img src="./img/stories_background.jpg" alt="borda do storie"/>
-                        <img src="./img/stories/6.png" alt="etbr"/>                    
-                    </div>
-                    <h1>etbr</h1>
-                </div>
-                <div className="story">
-                    <div>
-                        <img src="./img/stories_background.jpg" alt="borda do storie"/>
-                        <img src="./img/stories/7.png" alt="respondeai"/>                    
-                    </div>
-                    <h1>respondeai</h1>
-                </div>
-                <div className="story">
-                    <div>
-                        <img src="./img/stories_background.jpg" alt="borda do storie"/>
-                        <img src="./img/stories/8.png" alt="wawa"/>                    
-                    </div>
-                    <h1>wawa</h1>
-                </div>
-                <div className="seta">
+            {arrStory.map(
+                (p) => <Story borda={p.borda} imagem={p.imagem} nome={p.nome}  /> 
+            )}
+            <div className="seta">
                     <ion-icon name="chevron-forward-circle"></ion-icon>
-                </div>
-            </div>
+            </div> 
+        </div>
     )
 }
