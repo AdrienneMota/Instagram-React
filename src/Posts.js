28b3d8ff8fa5422bc49f134ead85fbb8325c6526
Adrienne Mota <adriennemota97@gmail.com>
2022-09-18 00:38:21 -0400

    usuario, salvar dinamico - feito

--- src/Posts.js
+++ src/Posts.js
@@ -1,84 +1,84 @@
+import React from "react"
+
+function Post(prop){
+    return(
+        <div className="post">
+                    <div className="nome-perfil">
+                        <div>
+                            <div className="titulo-perfil">
+                            <img src={prop.imagemPerfil} alt="nome-perfil"/>
+                            <p>{prop.nomePerfil}</p>
+                            </div>
+                            <ion-icon className="pontinhos" name="ellipsis-horizontal-outline"></ion-icon>
+                        </div>
+                    </div>
+                    <div className="post-midia">
+                        <img src={prop.imagemPost} />
+                    </div>
+                    <div className="icones-post">
+                        <div className="icons">
+                            <ion-icon name={prop.iconeCoracao}></ion-icon>
+                            <ion-icon name={prop.iconeChat}></ion-icon>
+                            <ion-icon name={prop.iconeEnviar}></ion-icon>
+                        </div>
+                        <div className="buttom-salvar" onClick={()=>{ prop.salvar(prop.chave) }}>
+                            <ion-icon name={prop.iconeSalvar}></ion-icon>
+                        </div>
+                    </div>
+                    <div className="curtido-por">
+                    <img src={prop.imagemCurtidospor}/>
+                    <p>Curtido por <strong>respondeai</strong> e <strong>outras {prop.qntCurtidospor} pessoas</strong></p>
+                    </div>
+                </div>
+    )
+}
+
 export default function Posts(){
+    
+    const listaPost = [
+        {
+            imagemPerfil: "./img/posts/post2/barked 2.png",
+            nomePerfil: "barked",
+            imagemPost: "./img/posts/post2/dog 1.png",
+            iconeCoracao: "heart-outline",
+            iconeChat: "chatbubble-outline",
+            iconeEnviar: "paper-plane-outline",
+            iconeSalvar: "bookmark-outline",
+            imagemCurtidospor: "./img/posts/post1/respondeai 2.png",
+            qntCurtidospor: "1",
+            key: +(new Date()),//criando um codigo para cada post
+            isSaved: false
+        }
+    ]
+    
+    const [lista, setlista] = React.useState(listaPost)
+    const onSave = (key)=>{
+        const index = lista.findIndex((p) =>  p.key === key)
+        if(index<0){
+            return
+        }
+        const post = lista[index]
+        post.isSaved = !post.isSaved
+
+        if(post.isSaved){
+            post.iconeSalvar = "bookmark"
+        }else{
+            post.iconeSalvar = "bookmark-outline"
+        }
+
+        const copyList = []
+        Object.assign(copyList, lista)
+        copyList[index] = post
+        setlista(copyList)
+    }
+
     return(
         <div className="posts">
-                <div className="post">
-                    <div className="nome-perfil">
-                        <div>
-                            <div className="titulo-perfil">
-                            <img src="./img/posts/post2/barked 2.png" alt="nome-perfil"/>
-                            <p>barked</p>
-                            </div>
-                            <ion-icon className="pontinhos" name="ellipsis-horizontal-outline"></ion-icon>
-                        </div>
-                    </div>
-                    <div className="post-midia">
-                        <img src="./img/posts/post2/dog 1.png"/>
-                    </div>
-                    <div className="icones-post">
-                        <div>
-                            <ion-icon name="heart-outline"></ion-icon>
-                            <ion-icon name="chatbubble-outline"></ion-icon>
-                            <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                            <ion-icon className="buttom-salvar" name="bookmark-outline"></ion-icon>
-                    </div>
-                    <div className="curtido-por">
-                    <img src="./img/posts/post1/respondeai 2.png"/>
-                    <p>Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong></p>
-                    </div>
-                </div>
-                <div className="post">
-                    <div className="nome-perfil">
-                        <div>
-                            <div className="titulo-perfil">
-                            <img src="./img/posts/post1/meowed 2.png" alt="nome-perfil"/>
-                            <p>meowed</p>
-                            </div>
-                            <ion-icon class="pontinhos" name="ellipsis-horizontal-outline"></ion-icon>
-                        </div>
-                    </div>
-                    <div className="post-midia">
-                        <img src="./img/posts/post1/gato-telefone 1.png" alt="gato-telefone"/>
-                    </div>
-                    <div className="icones-post">
-                        <div>
-                            <ion-icon name="heart-outline"></ion-icon>
-                            <ion-icon name="chatbubble-outline"></ion-icon>
-                            <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                            <ion-icon class="buttom-salvar" name="bookmark-outline"></ion-icon>
-                    </div>
-                    <div className="curtido-por">
-                    <img src="./img/posts/post1/respondeai 2.png"/>
-                    <p>Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong></p>
-                    </div>
-                </div>
-                <div className="post">
-                    <div className="nome-perfil">
-                        <div>
-                            <div className="titulo-perfil">
-                            <img src="./img/posts/post1/meowed 2.png" alt="nome-perfil"/>
-                            <p>meaow</p>
-                            </div>
-                            <ion-icon className="pontinhos" name="ellipsis-horizontal-outline"></ion-icon>
-                        </div>
-                    </div>
-                    <div className="post-midia">
-                        <img src="./img/posts/post1/gato-telefone 1.png" alt="gato-telefone"/>
-                    </div>
-                    <div className="icones-post">
-                        <div>
-                            <ion-icon name="heart-outline"></ion-icon>
-                            <ion-icon name="chatbubble-outline"></ion-icon>
-                            <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                            <ion-icon class="buttom-salvar" name="bookmark-outline"></ion-icon>
-                    </div>
-                    <div className="curtido-por">
-                    <img src="./img/posts/post1/respondeai 2.png"/>
-                    <p>Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong></p>
-                    </div>
-                </div>
+            {
+                lista.map(
+                    (p) => <Post imagemPerfil = {p.imagemPerfil} nomePerfil={p.nomePerfil} imagemPost = {p.imagemPost} iconeCoracao={p.iconeCoracao} iconeChat={p.iconeChat} iconeEnviar={p.iconeEnviar} iconeSalvar={p.iconeSalvar} imagemCurtidospor={p.imagemCurtidospor} qntCurtidospor = {p.qntCurtidospor} salvar={onSave} chave={p.key}/>
+                )
+            }        
         </div>
     )
 }
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,11 +1,50 @@
-export default function Usuario(){
-    return(
-    <div className="sugestao-principal">
-        <img src="./img/sidebar/catanacomics-principal.png" alt="sugestao-principal"/>
-        <div>
-            <p>catanacomics</p>
-            <p>Catana</p>
+import React from "react"
+
+function User(prop) {
+    return (
+        <div className="sugestao-principal">
+            <img src={prop.img} alt="sugestao-principal" width="56px" height="56px" onClick={prop.mudaImagem}/>
+            <div>
+                <p>catanacomics</p>
+                <p>{prop.name} <span onClick={prop.mudaNome}>mudanome</span></p>
+            </div>
         </div>
-    </div>
+    )
+}
+
+export default function Usuario() {
+    const defaultUser = { imgUser: "./img/sidebar/catanacomics-principal.png", nameUser: "Catana" }
+    const [user, setUser] = React.useState(defaultUser)
+
+    const changeUsername = () => {
+        const name = prompt("Digite seu nome: ")
+        if (!name) {
+            alert('Nome não válido')
+            return
+        }
+        const newUser = {}
+        Object.assign(newUser, defaultUser)
+        newUser.nameUser = name
+        setUser(newUser)
+        console.log(newUser)
+    }
+
+    const ChangeImagemUser = () => {
+        const img = prompt("Digite o caminho da imagem")
+        if(!img){
+            alert('A imagem não é válido')
+            return
+        }
+        const newUser = {}
+        Object.assign(newUser, defaultUser)
+        newUser.imgUser = img
+        setUser(newUser)
+        console.log(newUser)
+    }
+
+    return (
+        <div>
+            <User img={user.imgUser} name={user.nameUser} mudaNome={changeUsername} mudaImagem={ChangeImagemUser}/>
+        </div>
     )
 }
